dd46857071eee97d8958c4892b9da887741b822e
ShivanshKumarJha <kumarjhashivansh@gmail.com>
2023-08-30 17:00:55 +0530

    changed to run in own laptop

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,28 +2,28 @@
 const mongoose = require('mongoose');
 
 // connect to the database
-// mongoose.connect('mongodb://localhost/todos');
+mongoose.connect('mongodb://localhost/todos');
 
 // acquire the connection
-// const db = mongoose.connection;
+const db = mongoose.connection;
 
 // error
-// db.on('error', console.error.bind(console, 'Error in connecting to the MongoDB'));
+db.on('error', console.error.bind(console, 'Error in connecting to the MongoDB'));
 
 // Up and running then print the message
-// db.once('open', function () {
-//     console.log('Connected to the database');
-// })
+db.once('open', function () {
+    console.log('Connected to the database');
+})
 
 // for deploying the app
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_CONNECT_URI)
-        console.log("Connect to MongoDB successfully")
-    } catch (error) {
-        console.log("Connect failed " + error.message)
-    }
-}
+// const connectDB = async () => {
+//     try {
+//         await mongoose.connect(process.env.MONGODB_CONNECT_URI)
+//         console.log("Connect to MongoDB successfully")
+//     } catch (error) {
+//         console.log("Connect failed " + error.message)
+//     }
+// }
 
 // exporting the database
-module.exports = connectDB;
+module.exports = db;
